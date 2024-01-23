3cfba8453d9c6de7bf35462775bc8d07a98e6dee
Mohammad Iqbal <emon@ndessl.local>
2024-01-23 13:44:00 +0600

    1/23

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,4 +1,4 @@
-export const API_URL = 'https://localhost:7006';
-// export const API_URL = 'http://182.163.119.194:8081'; //live
+// export const API_URL = 'https://localhost:7006';
+export const API_URL = 'http://182.163.119.194:8081';
 // export const Payment_API_URL = 'http://182.163.119.194:8774';
 export const Payment_API_URL = 'http://182.163.119.194:8082'; //live
