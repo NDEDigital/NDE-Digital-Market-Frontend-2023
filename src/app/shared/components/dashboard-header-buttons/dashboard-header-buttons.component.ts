b9b9bbfd49701eacc6b2b28a5242c817ba34428a
Mahfuzul Alam <mahfuzul@ndessl.local>
2024-04-23 19:40:38 +0600

    some customized

---
+++ src/app/shared/components/dashboard-header-buttons/dashboard-header-buttons.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-header-buttons',
+  templateUrl: './dashboard-header-buttons.component.html',
+  styleUrls: ['./dashboard-header-buttons.component.css']
+})
+export class DashboardHeaderButtonsComponent {
+
+}
