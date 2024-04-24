1eefb2f0b004196c9e9d3b68bc904beb7bcf0171
Mahfuzul Alam <mahfuzul@ndessl.local>
2024-04-24 18:56:24 +0600

    Banner Approval Page updated

--- src/app/services/add-banner.service.ts
+++ src/app/services/add-banner.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { API_URL } from '../config';
 @Injectable({
   providedIn: 'root',
 })
 export class AddBannerService {
+  URL = API_URL;
   createBannerURL = 'https://localhost:7006/api/AddBanner/AddBanner';
   // deleteURL = 'https://localhost:7006/api/AddBanner/DeleteBanner/${bannerId}';
 
   editURL = 'https://localhost:7006/api/AddBanner/UpdateBanner';
 
   getURL = 'https://localhost:7006/api/AddBanner/GetAddBanner';
+  UpdateBannerStatusURL = `${this.URL}/api/AddBanner/UpdateBannerStatus`;
   constructor(private http: HttpClient) {}
 
   addBanner(formData: FormData): Observable<any> {
@@ -38,8 +41,21 @@ export class AddBannerService {
   }
 
   getBanner(status: any) {
-    return this.http.get(this.getURL, {
-      params: { status },
-    });
+    if (status == -1) {
+      return this.http.get(this.getURL);
+    } else if (status == 1) {
+      status = true;
+      return this.http.get(this.getURL, {
+        params: { status },
+      });
+    } else {
+      status = false;
+      return this.http.get(this.getURL, {
+        params: { status },
+      });
+    }
+  }
+  UpdateBannerStatus(formdata: any) {
+    return this.http.put(this.UpdateBannerStatusURL, formdata);
   }
 }
