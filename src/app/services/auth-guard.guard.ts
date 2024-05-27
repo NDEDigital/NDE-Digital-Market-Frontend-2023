32d1ed35a8d98fa9ee086b2c6aa4fd13c690e5d8
Mahfuzul Alam <mahfuzul@ndessl.local>
2024-05-27 12:43:07 +0600

    version changing

--- src/app/services/auth-guard.guard.ts
+++ src/app/services/auth-guard.guard.ts
@@ -1,18 +1,12 @@
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  Router,
-  RouterStateSnapshot,
-  UrlTree,
-} from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { SharedService } from './shared.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuardGuard implements CanActivate {
+export class AuthGuardGuard  {
   isLoggedIn = false;
   constructor(private sharedService: SharedService, private router: Router) {
     this.sharedService.loginStatus$.subscribe((isLoggedIn: boolean) => {
--- src/app/services/cant-reload.guard.ts
+++ src/app/services/cant-reload.guard.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CantReloadGuard implements CanActivate {
+export class CantReloadGuard  {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
