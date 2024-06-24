c1cdf7004881d9a1ddbea430497f568ac01712c3
Moinul Islam <moinul@ndessl.local>
2024-06-24 14:06:39 +0600

    group product filter page design improved

--- src/app/buyer/components/product-sidebar/product-sidebar.component.ts
+++ src/app/buyer/components/product-sidebar/product-sidebar.component.ts
@@ -60,8 +60,10 @@ export class ProductSidebarComponent implements OnInit {
 
   filteredProducts: any[] = [];
   filteredCompanyList: any[] = [];
+  filterContent: any[] = [];
   selectedGroup: string = '';
   groupSelected: string = '';
+  clear = true;
   @Input() showCategory!: boolean;
   @Input() showCompany!: boolean;
   @Input() showGroup!: boolean;
@@ -75,6 +77,9 @@ export class ProductSidebarComponent implements OnInit {
     this.loadCategory();
 
     this.activeEntry = localStorage.getItem('groupCodes');
+
+    this.filterContent = [];
+    this.filterData();
   }
 
   ngOnInit(): void {
@@ -83,6 +88,8 @@ export class ProductSidebarComponent implements OnInit {
     console.log(this.activeCompany);
 
     this.loadBrand();
+    this.filterContent = [];
+    this.filterData();
   }
   toggleGroupVisibility() {
     this.showGroupValue = !this.showGroupValue;
@@ -90,10 +97,45 @@ export class ProductSidebarComponent implements OnInit {
   toggleCompaniesVisibility() {
     this.showCompaniesValue = !this.showCompaniesValue;
   }
+  filterData() {
+    if (this.activeEntry) this.filterContent.push('Category');
+    if (this.activeCompany) this.filterContent.push('Company');
+    if (!this.activeEntry && !this.activeCompany) {
+      this.clear = false;
+    } else {
+      this.clear = true;
+    }
+  }
+  removeFilterContent(item: string) {
+    if (item == 'all') {
+      this.activeCompany = '';
+      this.activeEntry = '';
+      this.filterContent = [];
+      this.setGroupData(this.activeEntry, this.activeCompany);
+    }
+    if (item == 'Company') {
+      this.activeCompany = '';
+      this.setGroupData(this.activeEntry, this.activeCompany);
+    } else if (item == 'Category') {
+      this.activeEntry = '';
+      this.selectCompany(this.activeEntry, this.activeCompany);
+    }
+    const index = this.filterContent.indexOf(item);
+    if (index > -1) {
+      this.filterContent.splice(index, 1);
+    }
+    if (!this.activeEntry && !this.activeCompany) {
+      this.clear = false;
+    } else {
+      this.clear = true;
+    }
+  }
   selectCompany(companyName: string, companyCode: string) {
     this.selectedCompanyName = companyCode;
     this.activeCompany = companyCode;
     let groupCode = this.activeEntry;
+    this.filterContent = [];
+    this.filterData();
     console.log(this.activeEntry, 'Assscheee pore', this.activeCompany);
     this.companySelected.emit({ groupCode, companyCode });
   }
@@ -101,7 +143,8 @@ export class ProductSidebarComponent implements OnInit {
   setSelectData(groupCode: string, companyCode: string) {
     this.activeEntry = groupCode;
     console.log(this.activeEntry, 'ashcssssssssssssse');
-
+    this.filterContent = [];
+    this.filterData();
     this.loadCompanyList.emit({ groupCode, companyCode });
   }
 
@@ -110,8 +153,10 @@ export class ProductSidebarComponent implements OnInit {
     console.log('Assscheee pore', this.companyCode);
     this.selectedCompanyName = '';
     this.activeEntry = groupCode;
-    companyCode = '';
-    this.activeCompany = '';
+    companyCode = this.activeCompany;
+
+    this.filterContent = [];
+    this.filterData();
     console.log(groupCode, 'GROUP DTAAAA');
     this.getAllProduct.emit({ groupCode, companyCode });
   }
