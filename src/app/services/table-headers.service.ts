5a532b08c49e31b9232926e321cd36bd75ccce4f
NDEDigital <ndedigitalmarket@gmail.com>
2024-06-27 12:15:38 +0600

    company code hide

--- src/app/services/table-headers.service.ts
+++ src/app/services/table-headers.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 export class TableHeadersService {
   companyApprovalTableHeaders: string[] = [
     'Index',
-    'Company Code',
+    // 'Company Code',
     'Company Name',
     'Email',
     'Business Registration Number',
